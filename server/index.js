5fbc03f55f0312c8b5c39146269e6481aa11d944
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-12-30 17:36:02 +0200

    serv

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -14,19 +17,6 @@ const CONNECTION_URL =
 
 const PORT = process.env.PORT || 5000;
 
-// mongoose
-//   .connect(CONNECTION_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() =>
-//     app
-//       .listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
-//       .catch((error) => console.log(error.message))
-//   );
-
-// mongoose.set("useFindAndModify", false);
-
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
@@ -34,4 +24,4 @@ mongoose
   )
   .catch((error) => console.log(error.message));
 // mongoose.set("useFindAndModify", false);
-// mongoose.connect(CONNECTION_URL).then(()=>{console.log('...')})
+mongoose.set("strictQuery", false);
