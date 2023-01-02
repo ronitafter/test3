df26e6fd4cd6bd6b86eec9347fa55c35608e6e50
ronitafter <54575125+ronitafter@users.noreply.github.com>
2023-01-02 23:26:11 +0200

    new

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import memories from "./images/memories.png";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
 
 const App = () => {
   return (
-    <div>
-      <h1>hey</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          MEMO
+        </Typography>
+        <img src={memories} alt="memories" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
