88c073beb59dfae09e471e491a31e48d90c5777d
ronitafter <54575125+ronitafter@users.noreply.github.com>
2023-01-04 22:07:59 +0200

    redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { configureStore, applyMiddleware, compose } from "redux";
+// configureStore => createStore
+import thunk from "redux-thunk";
+import reducers from "./reducers";
 
 import App from "./App";
+
+const Store = configureStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
