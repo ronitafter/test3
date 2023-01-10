5fb7825cd1fe9866079ca29049ff83b9fe3f1839
RONIT AFTER <ronitafter@gmail.com>
2023-01-10 12:54:18 +0200

    new

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,15 +1,39 @@
 import React from "react";
 import useStyles from "./styles";
+import {
+  Card,
+  CardActions,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import ThumbUpIcon from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import moment from "moment";
 // import { useSelector } from "react-redux";
 
-const Post = () => {
+const Post = ({ post }) => {
   // const posts = useSelector((state) => state.posts);
   const classes = useStyles();
   // console.log(posts);
   return (
-    <div>
-      <h1>Post</h1>
-    </div>
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={post.selectedFile}
+        title={post.title}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button style={{ color: "white" }}></Button>
+      </div>
+    </Card>
   );
 };
 
