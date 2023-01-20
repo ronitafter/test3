44b834e86dbed61475178e4353f5aa379e80ba69
ronitafter <54575125+ronitafter@users.noreply.github.com>
2023-01-20 22:29:12 +0200

    newww

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+// import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+// dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -16,6 +18,7 @@ app.use("/posts", postRoutes);
 const CONNECTION_URL =
   "mongodb+srv://ra:ra123@cluster0.0vlk0nz.mongodb.net/?retryWrites=true&w=majority";
 
+// const PORT = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose
@@ -24,5 +27,4 @@ mongoose
     app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
   )
   .catch((error) => console.log(error.message));
-// mongoose.set("useFindAndModify", false);
 mongoose.set("strictQuery", false);
